e3bc32a5c7906b19db5f10a1a132d12c29c7a4fe
Sipi Krishna <sipikrishna@gmail.com>
2025-05-22 09:40:08 -0400

    Add files via upload

--- Manufacturing_Assets/machine_data_distinct_ingest.py
+++ Manufacturing_Assets/machine_data_distinct_ingest.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## machine_data_distinct_ingest
+# 
+# New notebook
+
+# In[ ]:
+
+
 import pandas as pd
 
 url = "https://raw.githubusercontent.com/centricconsulting/Fabric-in-a-day/main/Manufacturing_Assets/MachineDataDistinct.csv"
@@ -7,3 +17,21 @@ df = spark.createDataFrame(pdf)
 
 # 'Files/' maps to the default Lakehouse's file storage
 df.write.mode("overwrite").parquet("Files/MachineDataDistinct/")
+
+
+# In[ ]:
+
+
+df_machine_data = spark.read.format("parquet").option("header","true").load("abfss://Centric_Fabric_Workshop@onelake.dfs.fabric.microsoft.com/LitmusOEELH.Lakehouse/Files/MachineDataDistinct")
+
+df_machine_data_deduped = df_machine_data.dropDuplicates()
+df_machine_data_deduped.write.format("delta").option("overwriteSchema", "true").mode("overwrite").saveAsTable("dimassetitem")
+
+
+# In[ ]:
+
+
+# The command is not a standard IPython magic command. It is designed for use within Fabric notebooks only.
+# %%sql
+# select * from dimassetitem
+
